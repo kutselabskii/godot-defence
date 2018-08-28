addea774c4ffc70931de76be878b8f69ca68aaf9
kutselabskii <osutanfirst@gmail.com>
2018-08-28 19:22:33 +1000

    Change animation system
    Now uses AnimationPlayer and a Sprite instead of AnimatedSprite

--- scenes/Mob/Mob.gd
+++ scenes/Mob/Mob.gd
@@ -4,7 +4,7 @@ enum STATE {
 	IDLE,
 	WALK_RIGHT,
 	WALK_LEFT,
-	HAND
+	DYING
 }
 
 var modulations = [Color(0, 0, 255), Color(255, 255, 0), Color(255, 0, 0)]
@@ -18,8 +18,7 @@ func _ready():
 	randomize()
 	change_state()
 	switch_animation()
-	$AnimatedSprite.self_modulate = modulations[hit_points]
-	$AnimatedSprite.play()
+	$Sprite.self_modulate = modulations[hit_points]
 
 
 func _process(delta):
@@ -38,30 +37,36 @@ func start(starting_position = Vector2(150, 500)):
 
 func _on_Player_area_entered(area):
 	hit_points -= 1
-	$AnimatedSprite.self_modulate = modulations[hit_points]
-	
-	if hit_points == 0:
-		get_parent().create_mob()
-		queue_free()
+	$Sprite.self_modulate = modulations[hit_points]
+
+	if hit_points <= 0:
+		$StateTimer.stop()
+		state = STATE.DYING
+		$CollisionShape2D.disabled = true
+		$AnimationPlayer.play("Dying")
+		$Beep.play()
 	
 	
 func switch_animation():
 	if state == STATE.IDLE:
-		$AnimatedSprite.animation = "Idle"
+		$AnimationPlayer.play("Idle")
 	elif state == STATE.WALK_LEFT:
-		$AnimatedSprite.animation = "Left"
-		$AnimatedSprite.flip_h = false
+		$AnimationPlayer.play("Movement")
+		$Sprite.flip_h = false
 	elif state == STATE.WALK_RIGHT:
-		$AnimatedSprite.animation = "Left"
-		$AnimatedSprite.flip_h = true
-	elif state == STATE.HAND:
-		$AnimatedSprite.animation = "Rise hand"
+		$AnimationPlayer.play("Movement")
+		$Sprite.flip_h = true
 		
 func change_state():
 	var time = randf() + 1
-	var new_state = randi() % len(STATE)
+	var new_state = randi() % (len(STATE) - 1)
 	state = new_state
 	switch_animation()
 	$StateTimer.wait_time = time
 	$StateTimer.one_shot = true
 	$StateTimer.start()
+
+func _on_AnimationPlayer_animation_finished(anim_name):
+	if anim_name == "Dying":
+		get_parent().create_mob()
+		queue_free()
